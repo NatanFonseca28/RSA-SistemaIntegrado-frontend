06c3b03a1a904a535fc51d167bbbcb068e5a2c36
natanfonseca28 <natanfs28@gmail.com>
2021-03-16 20:05:06 -0300

    Atualização do global

--- src/global.js
+++ src/global.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 
 
 export const userKey = '__rsa_user'
-export const baseApiUrl = 'http://localhost:3000';
+    /* export const baseApiUrl = 'http://localhost:3000'; */
 
-/* export const baseApiUrl = 'https://sgi-rsa.herokuapp.com'; */
+export const baseApiUrl = 'https://sgi-rsa.herokuapp.com';
 export function showError(e) {
     if (e && e.response && e.response.data) {
         Vue.toasted.global.defaultError({
